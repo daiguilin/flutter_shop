233af00b83e44c63bfce9e456a45653d7ac5abf0
daiguilin <1535979269@qq.com>
2020-05-17 22:15:16 +0800

    前9节课程练习

---
+++ lib/config/service_url.dart
@@ -0,0 +1,10 @@
+/*
+ * @Author: daiGuilin
+ * @Date: 2020-05-17 21:41:36
+ * @LastEditTime: 2020-05-17 21:44:59
+ * @LastEditors: daiGuilin
+ */
+const serviceUrl = 'https://wxmini.baixingliangfan.cn/baixing/';
+const servicePath = {
+  'homePageContent': serviceUrl + 'wxmini/homePageContent', //商店首页信息
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,35 @@
 /*
  * @Author: daiGuilin
  * @Date: 2020-05-17 16:41:56
- * @LastEditTime: 2020-05-17 16:44:15
+ * @LastEditTime: 2020-05-17 22:05:37
  * @LastEditors: daiGuilin
  */
 import 'package:flutter/material.dart';
+import '../service/service_method.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String homePageContent = '正在获取数据';
+  @override
+  void initState() {
+    getHomePageContent().then((val) {
+      setState(() {
+        homePageContent = val.toString();
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('首页')),
+    return Container(
+      child: Scaffold(
+          appBar: AppBar(title: Text('美好人间')),
+          body: SingleChildScrollView(child: Text(homePageContent))),
     );
   }
 }
