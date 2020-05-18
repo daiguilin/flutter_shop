82ae68b395b48a79ff94fc5678a1bdf86473721e
daiguilin <1535979269@qq.com>
2020-05-18 21:11:22 +0800

    使用ScreenUtil插件进行屏幕适配

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,14 @@
 /*
  * @Author: daiGuilin
  * @Date: 2020-05-17 16:41:56
- * @LastEditTime: 2020-05-17 22:52:31
+ * @LastEditTime: 2020-05-18 21:06:59
  * @LastEditors: daiGuilin
  */
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../service/service_method.dart';
 
 class HomePage extends StatefulWidget {
@@ -22,24 +23,25 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Container(
         child: Scaffold(
-      appBar: AppBar(title: Text('百姓生活+')),
-      body: FutureBuilder(
-        future: getHomePageContent(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            var data = jsonDecode(snapshot.data.toString());
-            List<Map> swiper = (data['data']['slides'] as List).cast();
-            return Column(
-              children: <Widget>[SwiperDiy(swiperDateList: swiper)],
-            );
-          } else {
-            return Center(
-              child: Text('加载中。。。'),
-            );
-          }
-        },
-      ),
-    ));
+            appBar: AppBar(title: Text('百姓生活+')),
+            body: SingleChildScrollView(
+              child: FutureBuilder(
+                future: getHomePageContent(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    var data = jsonDecode(snapshot.data.toString());
+                    List<Map> swiper = (data['data']['slides'] as List).cast();
+                    return Column(
+                      children: <Widget>[SwiperDiy(swiperDateList: swiper)],
+                    );
+                  } else {
+                    return Center(
+                      child: Text('加载中。。。'),
+                    );
+                  }
+                },
+              ),
+            )));
   }
 }
 
@@ -48,10 +50,20 @@ class SwiperDiy extends StatelessWidget {
   final List swiperDateList;
 
   const SwiperDiy({Key key, this.swiperDateList}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(
+      context,
+      width: 750,
+      height: 1334,
+    );
+    print('设备的像素密度：${ScreenUtil.pixelRatio}');
+    print('设备的高：${ScreenUtil.screenHeight}');
+    print('设备的宽：${ScreenUtil.screenWidth}');
     return Container(
-      height: 333,
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
       child: Swiper(
         itemCount: 3,
         itemBuilder: ((BuildContext context, int index) {
